acc893ff4283dfc93aee94b2494738a590c639d8
Taciana Shimokawa <tmshimokawa@gmail.com>
2022-07-16 16:09:53 -0300

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'lists#home'
+  resources :lists do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
